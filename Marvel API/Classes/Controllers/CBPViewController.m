35c0032310632259c5eac21a64d8cc7de4a71aa5
Karl Monaghan <karl.t.monaghan@gmail.com>
2014-02-09 23:07:17 +0000

    Example is now last/this/next week comics search

--- Marvel API/Classes/Controllers/CBPViewController.m
+++ Marvel API/Classes/Controllers/CBPViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "CBPViewController.h"
+#import "CBPComicViewController.h"
 
 #import "NSURLSessionDataTask+MarvelDeveloperAPI.h"
 #import "UIImageView+AFNetworking.h"
@@ -18,8 +19,10 @@
 #import "MDASearchParameters.h"
 
 @interface CBPViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (strong, nonatomic) NSArray *comics;
+@property (strong, nonatomic) MDASearchParameters *search;
 @property (strong, nonatomic) UITableView *tableView;
-@property (strong, nonatomic) NSArray *items;
+@property (strong, nonatomic) UISegmentedControl *segmentedControl;
 @end
 
 @implementation CBPViewController
@@ -28,17 +31,48 @@
 {
     [super loadView];
     
-    self.tableView = [[UITableView alloc] initWithFrame:self.view.frame style:UITableViewStylePlain];
+    self.segmentedControl = [[UISegmentedControl alloc] initWithItems:@[NSLocalizedString(@"Last Week", nil), NSLocalizedString(@"This Week", nil), NSLocalizedString(@"Next Week", nil)]];
+    self.segmentedControl.selectedSegmentIndex = 1;
+    [self.segmentedControl addTarget:self action:@selector(searchChanged:) forControlEvents:UIControlEventValueChanged];
+    self.segmentedControl.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [self.view addSubview:self.segmentedControl];
+    
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectZero
+                                                  style:UITableViewStylePlain];
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
+    self.tableView.translatesAutoresizingMaskIntoConstraints = NO;
     
     [self.view addSubview:self.tableView];
+    
+    NSDictionary *views = @{@"segmentedControl": self.segmentedControl,
+                            @"tableView": self.tableView};
+    
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[tableView][segmentedControl]|"
+                                                                      options:0
+                                                                      metrics:nil
+                                                                        views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|[tableView]|"
+                                                                      options:0
+                                                                      metrics:nil
+                                                                        views:views]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.segmentedControl
+                                                          attribute:NSLayoutAttributeCenterX
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeCenterX
+                                                         multiplier:1.0f
+                                                           constant:0]];
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.search = [MDASearchParameters new];
+    self.search.dateDescriptor = @"thisWeek";
     
     [self reload];
     
@@ -48,32 +82,52 @@
                                                                              action:@selector(reload)];
 }
 
-- (void)didReceiveMemoryWarning
+#pragma mark -
+- (void)searchChanged:(UISegmentedControl *)segmentedControl
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    switch (segmentedControl.selectedSegmentIndex) {
+        case 0:
+            self.search.dateDescriptor = @"lastWeek";
+            break;
+        case 1:
+            self.search.dateDescriptor = @"thisWeek";
+            break;
+        case 2:
+            self.search.dateDescriptor = @"nextWeek";
+            break;
+        default:
+            break;
+    }
+    
+    self.comics = nil;
+    
+    [self.tableView reloadData];
+    
+    [self reload];
 }
 
-#pragma mark -
 - (void)reload
 {
-    MDASearchParameters *search = [MDASearchParameters new];
-    
     __weak typeof(self) blockSelf = self;
     
-    [NSURLSessionDataTask fetchComicsWithSearch:(MDASearchParameters *)search withhBlock:^(MDAComicDataWrapper *data, NSError *error) {
+    [NSURLSessionDataTask fetchComicsWithSearch:self.search
+                                     withhBlock:^(MDAComicDataWrapper *wrapper, NSError *error) {
         if (!error) {
-            blockSelf.items = data.data.results;
+            blockSelf.comics = wrapper.data.results;
             
             [blockSelf.tableView reloadData];
         } else {
             NSLog(@"error: %@", error);
         }
     }];
-
 }
 
 #pragma mark - UITableViewDataSource
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 75.0f;
+}
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ComicCell"];
@@ -81,24 +135,29 @@
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"ComicCell"];
     }
     
-    MDAComic *comic = self.items[indexPath.row];
+    MDAComic *comic = self.comics[indexPath.row];
     cell.textLabel.text = comic.title;
     cell.detailTextLabel.text = comic.descriptionText;
     
     [cell.imageView setImageWithURL:comic.thumbnail.url
-                   placeholderImage:nil];
-    
+                   placeholderImage:[UIImage imageNamed:@"default"]];
+    cell.imageView.contentMode = UIViewContentModeScaleAspectFit;
+
     return cell;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return [self.items count];
+    return [self.comics count];
 }
 
 #pragma mark - UITableViewDelegate
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    [[tableView cellForRowAtIndexPath:indexPath] setSelected:NO animated:YES];
     
+    CBPComicViewController *vc = [[CBPComicViewController alloc] initWithComic:self.comics[indexPath.row]];
+    
+    [self.navigationController pushViewController:vc animated:YES];
 }
 @end
