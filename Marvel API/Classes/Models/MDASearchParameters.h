9e835c298fbecf4da33cf287b7606c43e2e4acf1
Karl Monaghan <karl.t.monaghan@gmail.com>
2014-02-10 08:08:05 +0000

    expanded out search object to all possible parameters

--- Marvel API/Classes/Models/MDASearchParameters.h
+++ Marvel API/Classes/Models/MDASearchParameters.h
@@ -7,23 +7,25 @@
 //
 
 @interface MDASearchParameters : NSObject
-@property (strong, nonatomic) NSString *format;
-@property (strong, nonatomic) NSString *formatType;
-@property (assign, nonatomic) BOOL noVariants;
+@property (strong, nonatomic) NSArray *characters;
+@property (strong, nonatomic) NSArray *collaborators;
+@property (strong, nonatomic) NSArray *creators;
 @property (strong, nonatomic) NSString *dateDescriptor;
 @property (strong, nonatomic) NSArray *dateRange;
-@property (assign, nonatomic) BOOL hasDigitalIssue;
-@property (strong, nonatomic) NSDate *modifiedSince;
-@property (strong, nonatomic) NSArray *creators;
-@property (strong, nonatomic) NSArray *characters;
-@property (strong, nonatomic) NSArray *series;
 @property (strong, nonatomic) NSArray *events;
+@property (strong, nonatomic) NSString *format;
+@property (strong, nonatomic) NSString *formatType;
+@property (assign, nonatomic) BOOL hasDigitalIssue;
+@property (assign, nonatomic) NSInteger limit;
+@property (strong, nonatomic) NSDate *modifiedSince;
+@property (strong, nonatomic) NSString *name;
+@property (assign, nonatomic) BOOL noVariants;
+@property (strong, nonatomic) NSArray *series;
 @property (strong, nonatomic) NSArray *stories;
 @property (strong, nonatomic) NSArray *sharedAppearances;
-@property (strong, nonatomic) NSArray *collaborators;
-@property (strong, nonatomic) NSString *orderBy;
-@property (assign, nonatomic) NSInteger limit;
 @property (assign, nonatomic) NSInteger offset;
+@property (strong, nonatomic) NSString *orderBy;
 
 - (NSDictionary *)parameters;
+
 @end
--- Marvel API/Classes/Models/MDASearchParameters.m
+++ Marvel API/Classes/Models/MDASearchParameters.m
@@ -13,8 +13,72 @@
 {
     NSMutableDictionary *params = @{}.mutableCopy;
     
+    if (self.format) {
+        params[@"format"] = self.format;
+    }
+    
+    if (self.formatType) {
+        params[@"formatType"] = self.formatType;
+    }
+    
+    if (self.noVariants) {
+        params[@"noVariants"] = @"true";
+    }
+    
     if (self.dateDescriptor) {
         params[@"dateDescriptor"] = self.dateDescriptor;
+    }
+    
+    if ([self.dateRange count]) {
+        params[@"dateRange"] = [self.dateRange componentsJoinedByString:@","];
+    }
+    
+    if (self.hasDigitalIssue) {
+        params[@"hasDigitalIssue"] = @"true";
+    }
+    
+    if (self.modifiedSince) {
+        params[@"modifiedSince"] = @"true";
+    }
+    
+    if ([self.creators count]) {
+        params[@"creators"] = [self.creators componentsJoinedByString:@","];
+    }
+    
+    if (self.characters) {
+        params[@"characters"] = [self.characters componentsJoinedByString:@","];
+    }
+    
+    if (self.series) {
+        params[@"series"] = [self.series componentsJoinedByString:@","];
+    }
+    
+    if (self.events) {
+        params[@"events"] = [self.events componentsJoinedByString:@","];
+    }
+    
+    if (self.stories) {
+        params[@"stories"] = [self.stories componentsJoinedByString:@","];
+    }
+    
+    if (self.sharedAppearances) {
+        params[@"sharedAppearances"] = [self.sharedAppearances componentsJoinedByString:@","];
+    }
+
+    if (self.collaborators) {
+        params[@"collaborators"] = [self.collaborators componentsJoinedByString:@","];
+    }
+    
+    if (self.orderBy) {
+        params[@"orderBy"] = self.orderBy;
+    }
+    
+    if (self.limit) {
+        params[@"limit"] = [NSNumber numberWithInteger:self.limit];
+    }
+    
+    if (self.offset) {
+        params[@"offset"] = [NSNumber numberWithInteger:self.offset];
     }
     
     return params;
