35a0bcb3b352504e64c1c276a0c7c0cf0238bf45
Karl Monaghan <karl.t.monaghan@gmail.com>
2014-02-13 15:57:47 +0000

    Formatting

--- Marvel API/Classes/Models/MDASeriesList.m
+++ Marvel API/Classes/Models/MDASeriesList.m
@@ -1,6 +1,6 @@
 //
 //  MDASeriesList.m
-//  
+//
 //
 //  Created by Karl Monaghan on 09/02/2014.
 //  Copyright (c) 2014 Crayons and Brown Paper. All rights reserved.
@@ -14,68 +14,68 @@
 
 + (MDASeriesList *)initFromDictionary:(NSDictionary *)aDictionary
 {
-
+    
     MDASeriesList *instance = [[MDASeriesList alloc] init];
     [instance setAttributesFromDictionary:aDictionary];
     return instance;
-
+    
 }
 
 - (void)setAttributesFromDictionary:(NSDictionary *)aDictionary
 {
-
+    
     if (![aDictionary isKindOfClass:[NSDictionary class]]) {
         return;
     }
-
+    
     [self setValuesForKeysWithDictionary:aDictionary];
-
+    
 }
 
 - (void)setValue:(id)value forKey:(NSString *)key
 {
-
+    
     if ([key isEqualToString:@"items"]) {
-
+        
         if ([value isKindOfClass:[NSArray class]])
-{
-
+        {
+            
             NSMutableArray *myMembers = [NSMutableArray arrayWithCapacity:[value count]];
             for (id valueMember in value) {
                 MDASeriesSummary *populatedMember = [MDASeriesSummary initFromDictionary:valueMember];
                 [myMembers addObject:populatedMember];
             }
-
+            
             self.items = myMembers;
-
+            
         }
-
+        
     } else {
         [super setValue:value forKey:key];
     }
-
+    
 }
 
 
 - (NSDictionary *)dictionaryRepresentation
 {
-
+    
     NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
-
+    
     [dictionary setObject:[NSNumber numberWithInteger:self.available] forKey:@"available"];
-
+    
     if (self.collectionURI) {
         [dictionary setObject:self.collectionURI forKey:@"collectionURI"];
     }
-
+    
     if (self.items) {
         [dictionary setObject:self.items forKey:@"items"];
     }
-
+    
     [dictionary setObject:[NSNumber numberWithInteger:self.returned] forKey:@"returned"];
-
+    
     return dictionary;
-
+    
 }
 
 @end
