fe94a91baf0306774fe35eaf3b09de81fec5de1c
Karl Monaghan <karl.t.monaghan@gmail.com>
2014-03-09 18:25:25 +0000

    Added new search parameters

--- MDAMarvelAPI/MDASearchParameters.h
+++ MDAMarvelAPI/MDASearchParameters.h
@@ -13,19 +13,33 @@
 @property (strong, nonatomic) NSArray *creators;
 @property (strong, nonatomic) NSString *dateDescriptor;
 @property (strong, nonatomic) NSArray *dateRange;
+@property (strong, nonatomic) NSString *diamondCode;
+@property (strong, nonatomic) NSString *digitalId;
+@property (strong, nonatomic) NSString *ean;
 @property (strong, nonatomic) NSArray *events;
+@property (strong, nonatomic) NSString *firstNameStartsWith;
 @property (strong, nonatomic) NSString *format;
 @property (strong, nonatomic) NSString *formatType;
 @property (assign, nonatomic) BOOL hasDigitalIssue;
+@property (strong, nonatomic) NSString *isbn;
+@property (strong, nonatomic) NSString *issn;
+@property (strong, nonatomic) NSString *lastNameStartsWith;
 @property (assign, nonatomic) NSInteger limit;
+@property (strong, nonatomic) NSString *middleNameStartsWith;
 @property (strong, nonatomic) NSDate *modifiedSince;
 @property (strong, nonatomic) NSString *name;
-@property (assign, nonatomic) BOOL noVariants;  
-@property (strong, nonatomic) NSArray *series;
-@property (strong, nonatomic) NSArray *stories;
-@property (strong, nonatomic) NSArray *sharedAppearances;
+@property (strong, nonatomic) NSString *nameStartsWith;
+@property (assign, nonatomic) BOOL noVariants;
 @property (assign, nonatomic) NSInteger offset;
 @property (strong, nonatomic) NSString *orderBy;
+@property (strong, nonatomic) NSArray *series;
+@property (strong, nonatomic) NSArray *sharedAppearances;
+@property (assign, nonatomic) NSInteger startYear;
+@property (strong, nonatomic) NSArray *stories;
+@property (strong, nonatomic) NSString *title;
+@property (strong, nonatomic) NSString *titleStartsWith;
+@property (strong, nonatomic) NSString *upc;
+
 
 - (NSDictionary *)parameters;
 
--- MDAMarvelAPI/MDASearchParameters.m
+++ MDAMarvelAPI/MDASearchParameters.m
@@ -13,16 +13,20 @@
 {
     NSMutableDictionary *params = @{}.mutableCopy;
     
-    if (self.format) {
-        params[@"format"] = self.format;
+    if (self.characters) {
+        params[@"characters"] = [self.characters componentsJoinedByString:@","];
     }
     
-    if (self.formatType) {
-        params[@"formatType"] = self.formatType;
+    if (self.collaborators) {
+        params[@"collaborators"] = [self.collaborators componentsJoinedByString:@","];
     }
     
-    if (self.noVariants) {
-        params[@"noVariants"] = @"true";
+    if ([self.comics count]) {
+        params[@"comics"] = [self.comics componentsJoinedByString:@","];
+    }
+    
+    if ([self.creators count]) {
+        params[@"creators"] = [self.creators componentsJoinedByString:@","];
     }
     
     if (self.dateDescriptor) {
@@ -33,56 +37,104 @@
         params[@"dateRange"] = [self.dateRange componentsJoinedByString:@","];
     }
     
-    if (self.hasDigitalIssue) {
-        params[@"hasDigitalIssue"] = @"true";
+    if (self.diamondCode) {
+        params[@"diamondCode"] = self.diamondCode;
     }
     
-    if (self.modifiedSince) {
-        params[@"modifiedSince"] = @"true";
+    if (self.digitalId) {
+        params[@"digitalId"] = self.digitalId;
     }
     
-    if ([self.creators count]) {
-        params[@"creators"] = [self.creators componentsJoinedByString:@","];
-    }
-    
-    if (self.characters) {
-        params[@"characters"] = [self.characters componentsJoinedByString:@","];
-    }
-    
-    if (self.series) {
-        params[@"series"] = [self.series componentsJoinedByString:@","];
+    if (self.ean) {
+        params[@"ean"] = self.ean;
     }
     
     if (self.events) {
         params[@"events"] = [self.events componentsJoinedByString:@","];
     }
     
-    if (self.stories) {
-        params[@"stories"] = [self.stories componentsJoinedByString:@","];
+    if (self.firstNameStartsWith) {
+        params[@"firstNameStartsWith"] = self.firstNameStartsWith;
     }
     
-    if (self.sharedAppearances) {
-        params[@"sharedAppearances"] = [self.sharedAppearances componentsJoinedByString:@","];
-    }
-
-    if (self.collaborators) {
-        params[@"collaborators"] = [self.collaborators componentsJoinedByString:@","];
+    if (self.format) {
+        params[@"format"] = self.format;
     }
     
-    if (self.orderBy) {
-        params[@"orderBy"] = self.orderBy;
+    if (self.formatType) {
+        params[@"formatType"] = self.formatType;
+    }
+    
+    if (self.hasDigitalIssue) {
+        params[@"hasDigitalIssue"] = @"true";
+    }
+    
+    if (self.isbn) {
+        params[@"isbn"] = self.isbn;
+    }
+    
+    if (self.issn) {
+        params[@"issn"] = self.issn;
+    }
+    
+    if (self.lastNameStartsWith) {
+        params[@"lastNameStartsWith"] = self.lastNameStartsWith;
     }
     
     if (self.limit) {
         params[@"limit"] = [NSNumber numberWithInteger:self.limit];
     }
     
+    if (self.middleNameStartsWith) {
+        params[@"middleNameStartsWith"] = self.middleNameStartsWith;
+    }
+    
+    if (self.modifiedSince) {
+        params[@"modifiedSince"] = @"true";
+    }
+    
+    if (self.name) {
+        params[@"name"] = self.name;
+    }
+    
+    if (self.nameStartsWith) {
+        params[@"nameStartsWith"] = self.nameStartsWith;
+    }
+    
+    if (self.noVariants) {
+        params[@"noVariants"] = @"true";
+    }
+    
     if (self.offset) {
         params[@"offset"] = [NSNumber numberWithInteger:self.offset];
     }
     
-    if ([self.comics count]) {
-        params[@"comics"] = [self.dateRange componentsJoinedByString:@","];
+    if (self.orderBy) {
+        params[@"orderBy"] = self.orderBy;
+    }
+    
+    if (self.series) {
+        params[@"series"] = [self.series componentsJoinedByString:@","];
+    }
+    
+    if (self.sharedAppearances) {
+        params[@"sharedAppearances"] = [self.sharedAppearances componentsJoinedByString:@","];
+    }
+    
+    if (self.stories) {
+        params[@"stories"] = [self.stories componentsJoinedByString:@","];
+    }
+
+    if (self.title) {
+        params[@"title"] = self.title;
+    }
+    
+    if (self.titleStartsWith) {
+        params[@"titleStartsWith"] = self.titleStartsWith;
+    }
+    
+    if (self.upc) {
+        params[@"upc"] = self.upc;
     }
     
     return params;
